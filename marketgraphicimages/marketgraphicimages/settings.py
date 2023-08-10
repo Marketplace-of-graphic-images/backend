dfee8e9531ee25340a7bf00a1914fb61aac2f2c1
Пётр Воложанин <mister.volojanin@yandex.ru>
2023-08-10 17:25:01 +0800

    Add pre-commit

--- marketgraphicimages/marketgraphicimages/settings.py
+++ marketgraphicimages/marketgraphicimages/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-c+@7f59vob3j5knjj&q)2btv2xx3985g@4rb1b4%jak#vyw1wc'
+SECRET_KEY = 'django-insecure-c+@7f59vob3j5knjj&q)2btv2xx3985g@4rb1b4%jak#vyw1wc' # @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
