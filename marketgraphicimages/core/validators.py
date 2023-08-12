87c5b926fbfd4a730ef1664498b565bdf8f0abc6
Пётр Воложанин <mister.volojanin@yandex.ru>
2023-08-12 16:42:03 +0800

    small fix

--- marketgraphicimages/core/validators.py
+++ marketgraphicimages/core/validators.py
@@ -4,19 +4,6 @@ from django.core.exceptions import ValidationError
 
 
 def date_is_past(value: date) -> None:
-    """
-    Validates whether a given date is in the past.
-
-    Args:
-        value (date): The date to be validated.
-
-    Raises:
-        ValidationError: If the input date is not in the past
-        or if the year is before 1900.
-
-    Returns:
-        None
-    """
     if isinstance(value, date):
         if value >= date.today() or value.year < 1900:
             raise ValidationError(
