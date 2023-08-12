d99cca2437e261f9e49d40ec29debbb314b2267c
Пётр Воложанин <mister.volojanin@yandex.ru>
2023-08-12 16:44:40 +0800

    small fix

--- marketgraphicimages/users/models.py
+++ marketgraphicimages/users/models.py
@@ -1,9 +1,10 @@
-from core.validators import date_is_past
 from django.contrib.auth.models import AbstractUser, UserManager
 from django.contrib.auth.validators import ASCIIUsernameValidator
 from django.core.validators import MinLengthValidator
 from django.db import models
 from django.utils.translation import gettext_lazy as _
+
+from core.validators import date_is_past
 
 
 class CustomUserManager(UserManager):
