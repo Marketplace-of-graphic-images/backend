bb47fcdce77d5e94b696ccdea92c5ef89d00584f
Petr Volozhanin <110925253+V1cimus@users.noreply.github.com>
2023-08-23 22:17:20 +0700

    return back google img

--- marketgraphicimages/core/utils.py
+++ marketgraphicimages/core/utils.py
@@ -1,3 +1,4 @@
+import io
 import random
 from random import randint
 
@@ -6,8 +7,9 @@ from django.contrib.auth import get_user_model
 from django.contrib.auth.tokens import PasswordResetTokenGenerator
 from django.core.mail import send_mail
 from django.shortcuts import get_object_or_404
-
+from google_images_search import GoogleImagesSearch
 from passlib.context import CryptContext
+from PIL import Image
 
 User = get_user_model()
 
@@ -81,3 +83,31 @@ class SixDigitCodeGenerator(PasswordResetTokenGenerator):
 
 
 six_digit_code_generator = SixDigitCodeGenerator()
+
+
+def get_img_from_google(search_name: str = 'Природа'):
+
+    API_KEY = ''
+    PROJECT_CX = ''
+    gis = GoogleImagesSearch(API_KEY, PROJECT_CX)
+
+    _search_params = {
+        'q': search_name,
+        'num': 10,
+    }
+
+    gis.search(search_params=_search_params)
+    return gis
+
+
+def show_img(gis):
+    my_bytes_io = io.BytesIO()
+
+    for image in gis.results():
+        my_bytes_io.seek(0)
+        raw_image_data = image.get_raw_data()
+        image.copy_to(my_bytes_io, raw_image_data)
+        image.copy_to(my_bytes_io)
+        my_bytes_io.seek(0)
+        temp_img = Image.open(my_bytes_io)
+        temp_img.show()
