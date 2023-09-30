9a6531d9c2f0bf1c0254d0d861ff5a0800e5039d
vlad3069 <vlad9603@gmail.com>
2023-09-30 09:48:24 +0300

    Лк другого пользователя

--- marketgraphicimages/core/permissions.py
+++ marketgraphicimages/core/permissions.py
@@ -26,3 +26,13 @@ class IsAuthorOrAdminPermission(permissions.BasePermission):
         return (request.user.is_authenticated
                 and request.user.is_author
                 or request.user.is_superuser)
+
+
+class CurrentUserOrReadOnlyOrAdmin(permissions.IsAuthenticated):
+    """Only the current user or read only or admin."""
+    def has_object_permission(self, request, view, obj):
+        user = request.user
+        return (user.is_superuser
+                or obj.pk == user.pk
+                or request.method == 'GET'
+                )
