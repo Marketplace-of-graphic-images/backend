9d4b8f96a125c717760dcd06b9ba65fa7ab74e4d
Vadim Yakhin <yakhin.v.r@gmail.com>
2023-10-04 07:49:27 +0500

    add image filter by name

--- marketgraphicimages/api/v1/filters.py
+++ marketgraphicimages/api/v1/filters.py
@@ -1,3 +1,4 @@
+from django.db.models import Q
 from django_filters.rest_framework import FilterSet, filters
 
 from images.models import Image
@@ -16,14 +17,17 @@ class ImageFilter(FilterSet):
     Parameters:
     - Filter by tags.
     - Filter by category based on format of image.
+    - FIlter by name. Double filtering: by entering the beginning of the name,
+    by entering an arbitrary place.
     """
 
     tags = filters.AllValuesMultipleFilter(field_name='tags__slug')
     category = filters.CharFilter(method='filter_category',)
+    name = filters.CharFilter(method='filter_name',)
 
     class Meta:
         model = Image
-        fields = ('tags', 'category',)
+        fields = ('tags', 'category', 'name',)
 
     def filter_category(self, queryset, _, value):
         """
@@ -33,3 +37,11 @@ class ImageFilter(FilterSet):
         if value in REGAX_PATTERNS:
             return queryset.filter(image__regex=REGAX_PATTERNS.get(value))
         return queryset
+
+    def filter_name(self, queryset, _, value):
+        """
+        Filters the specified `queryset` of queries based on the match with
+        the beginning of the name, then by a match anywhere in the name.
+        """
+        return queryset.filter(Q(name__icontains=value) | Q(
+            name__startswith=value)).distinct()
