86732c6988317d01618e341958465ca389e90df9
alexeypydev <kazus.lesha@yandex.ru>
2023-10-05 18:46:36 +0300

    fixed raises

--- marketgraphicimages/core/validators.py
+++ marketgraphicimages/core/validators.py
@@ -16,10 +16,11 @@ def date_is_past(value: date) -> None:
         raise ValidationError(message)
 
 def validate_email(email: str) -> None:
+    errors = []
     regex = re.compile(
         r'([A-Za-z0-9]+[.-_])*[A-Za-z0-9]+@[A-Za-z0-9-]+(\.[A-Z|a-z]{2,})+')
     if not re.fullmatch(regex, email):
-        raise ValidationError('Invalid email')
+        errors.append('Invalid email')
     # try:
     local_area, domain_area = email.split('@')
     # except ValueError:
@@ -28,15 +29,17 @@ def validate_email(email: str) -> None:
     #             code=status.HTTP_400_BAD_REQUEST,
     #     )
     if len(local_area) < 6 or len(local_area) > 64:
-        raise ValidationError('Allowed number of characters in the local'
+        errors.append('Allowed number of characters in the local'
                               ' area: more than 5 and less than 65')
     elif len(domain_area) > 253:
-        raise ValidationError('Allowed number of characters in the domain'
+        errors.append('Allowed number of characters in the domain'
                               ' area: less than 254')
     elif len(email) > 256:
-        raise ValidationError('Maximum email length 256 characters')
+        errors.append('Maximum email length 256 characters')
     # elif local_area[0] == '.' or local_area[len(local_area) - 1] == '.':
     #     raise ValidationError('A local area cannot begin or end with .')
     # elif local_area.count('.') > 1:
     #     if local_area[local_area.find('.') + 1] == '.':
     #         raise ValidationError(". can't repeated more than twice in a row")
+    if errors:
+        raise ValidationError(errors)
