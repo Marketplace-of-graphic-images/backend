2c30d76061df12b1d549d10fd4f6121978474f36
Воложанин Пётр <mister.volojanin@yandex.ru>
2023-11-12 13:47:06 +0700

    add author in RM

--- marketgraphicimages/marketgraphicimages/settings.py
+++ marketgraphicimages/marketgraphicimages/settings.py
@@ -297,6 +297,7 @@ CORS_ALLOW_CREDENTIALS = True
 
 CORS_ORIGIN_WHITELIST = (
     'http://localhost:3000',
+    'http://localhost:80',
     'https://marketplace-of-graphic-images.github.io',
     'http://graphic-images.lex-v.ru',
     'https://graphic-images.lex-v.ru',
@@ -313,12 +314,14 @@ CORS_ALLOW_METHODS = (
 
 CORS_ALLOWED_ORIGINS = (
     'http://localhost:3000',
+    'http://localhost:80',
     'https://marketplace-of-graphic-images.github.io',
     'http://graphic-images.lex-v.ru',
     'https://graphic-images.lex-v.ru',
 )
 CORS_ALLOWED_ORIGIN_REGEXES = (
     'http://localhost:3000',
+    'http://localhost:80',
     'https://marketplace-of-graphic-images.github.io',
     'http://graphic-images.lex-v.ru',
     'https://graphic-images.lex-v.ru',
